981e425cf1321ba2fcc053fa57833273e2a61761
marcelldac <devmarcell@protonmail.com>
2024-07-27 23:33:39 -0300

    feat: import entities module and type orm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    PostsModule,
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
