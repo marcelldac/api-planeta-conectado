f7f2fad8dcde4b82c4da8530475e92ecda2ea3ca
marcelldac <devmarcell@protonmail.com>
2024-07-27 23:38:14 -0300

    feat: use config service env variables

--- src/config/postgres.config.service.ts
+++ src/config/postgres.config.service.ts
@@ -1,15 +1,20 @@
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Post } from 'src/posts/entities/post.entity';
+import { User } from 'src/users/entities/user.entity';
 
 export class PostgresConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
   createTypeOrmOptions(): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: '127.0.0.1',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'planeta_conectado',
-      entities: [],
+      host: this.configService.get('DB_HOST'),
+      port: this.configService.get('DB_PORT'),
+      username: this.configService.get('DB_USER'),
+      password: this.configService.get('DB_PASS'),
+      database: this.configService.get('DB_NAME'),
+      entities: [User, Post],
       synchronize: true,
     };
   }
