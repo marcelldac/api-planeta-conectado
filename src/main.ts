9551b014e0db3d91b5f33f8954b52b7150e29ded
marcelldac <devmarcell@protonmail.com>
2024-07-28 16:37:59 -0300

    style: add a line

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
     .setTitle('API Planeta Conectado')
     .setVersion('1.0.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
